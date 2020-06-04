6dee9fbed5284a752870175d8aafe54ba67df79a
Janet <jaurinu@gmail.com>
2020-06-04 06:54:32 -0500

    Updates

---
+++ precache-manifest.ddb93cb2f29815af9db0c20a4ee71a10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7f77ac5081619f50a508027a66335b",
+    "url": "/Edilar/index.html"
+  },
+  {
+    "revision": "499b78db417f453ed453",
+    "url": "/Edilar/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1b94f4bb08262ba34f3d",
+    "url": "/Edilar/static/css/main.6fe2db4c.chunk.css"
+  },
+  {
+    "revision": "499b78db417f453ed453",
+    "url": "/Edilar/static/js/2.d2358cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Edilar/static/js/2.d2358cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b94f4bb08262ba34f3d",
+    "url": "/Edilar/static/js/main.912c6867.chunk.js"
+  },
+  {
+    "revision": "48546ca32a72fd45773f",
+    "url": "/Edilar/static/js/runtime-main.c54b7b27.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.com/Edilar/precache-manifest.9fead61aca66f33b3b79b52594ca9aef.js"
+  "/Edilar/precache-manifest.ddb93cb2f29815af9db0c20a4ee71a10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/Edilar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Edilar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
