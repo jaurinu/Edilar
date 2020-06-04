08cc288ededd06c273ce4ec2d8b7f25352a1ffeb
Janet <jaurinu@gmail.com>
2020-06-04 06:42:29 -0500

    Updates

---
+++ precache-manifest.a2bd9ff8e2c41f4794fd7e0e8d00d78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb66d9612cd8e9effa9c09281de5f947",
+    "url": "/jaurinu/Edilar/index.html"
+  },
+  {
+    "revision": "499b78db417f453ed453",
+    "url": "/jaurinu/Edilar/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1b94f4bb08262ba34f3d",
+    "url": "/jaurinu/Edilar/static/css/main.6fe2db4c.chunk.css"
+  },
+  {
+    "revision": "499b78db417f453ed453",
+    "url": "/jaurinu/Edilar/static/js/2.d2358cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jaurinu/Edilar/static/js/2.d2358cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b94f4bb08262ba34f3d",
+    "url": "/jaurinu/Edilar/static/js/main.912c6867.chunk.js"
+  },
+  {
+    "revision": "3e83e91ebf4ed334b1bc",
+    "url": "/jaurinu/Edilar/static/js/runtime-main.a78327de.js"
+  }
+]);
